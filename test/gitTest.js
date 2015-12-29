e7e34c3f91f8099e1b7bf54a7a416b3ed6fb1e51
Marcin Baniowski <marcin.baniowski@gmail.com>
2015-12-29 23:48:17 +0100

    Finish tests for git api

--- test/gitTest.js
+++ test/gitTest.js
@@ -20,7 +20,7 @@ describe('Git module tests', function () {
     describe('Get path to the repository with existing repo name', function () {
         it('should return the correct path', function () {
             var repoPath = git.getPath('Repo 1');
-            var testPath = path.resolve(path.join(__dirname, '../test/test-repo'));
+            var testPath = path.resolve(path.join(__dirname, '../test/repo-test'));
             assert.strictEqual(repoPath, testPath);
         });
     });
@@ -39,7 +39,6 @@ describe('Git module tests', function () {
         });
         it('first branch should be branch_1, second branch_2, third * master', function (done) {
             git.getBranches('Repo 1').then(branches => {
-                //console.log(branches)
                 assert.equal(branches[0], '  branch_1');
                 assert.equal(branches[1], '  branch_2');
                 assert.equal(branches[2], '* master');
@@ -68,6 +67,38 @@ describe('Git module tests', function () {
         it('should return latest git log', function (done) {
             git.getLatestLog('Repo 1').then(info => {
                 assert.equal(info, '14de4a8 first commit\n');
+                done();
+            });
+        });
+    });
+    describe('Get Repo Info', function () {
+        it('should return branches, current branch and latest log info', function (done) {
+            git.getRepoInfo('Repo 1').then(data => {
+                //console.log(data[0][0]);
+                assert.equal(data[0][0], '  branch_1');
+                assert.equal(data[1], 'On branch master');
+                assert.equal(data[2], '14de4a8 first commit\n');
+                done();
+            });
+        });
+    });
+    describe('Run hooks', function () {
+        it('should return first hook output', function (done) {
+            var hooks = git.execHooks('Repo 1');
+            hooks[0].then(data => {
+                assert.equal(data.trim(), 'test string');
+                done();
+            });
+        });
+    });
+    describe('Checkout', function () {
+        it('should return checked out branch info', function (done) {
+            var p = git.checkout('Repo 1', 'branch_1');
+            p.then(data => {
+                //assert.isArray(data);
+                assert.equal(data[0], 'On branch branch_1');
+                assert.equal(data[1], '14de4a8 first commit\n');
+                assert.equal(data[2], 'test string\n');
                 done();
             });
         });
--- test/test.sh
+++ test/test.sh
@@ -2,3 +2,5 @@
 
 tar xfz test/repo-test/git.tar.gz -C test/repo-test
 node_modules/mocha/bin/mocha test
+rm -fr test/repo-test/.git
+rm test/repo-test/index.js
